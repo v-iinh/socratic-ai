c4044cec671784f6fee42c88a8636d1501f00c79
v-iinh <officiallyvt@gmail.com>
2024-10-07 22:34:37 -0400

    pages setup

---
+++ scripts/pages/home.js
@@ -0,0 +1,23 @@
+(function() {
+  const headings = document.querySelectorAll('h2');
+  Array.prototype.forEach.call(headings, h => {
+    let btn = h.querySelector('button');
+    if (!btn) return; 
+    let target = h.nextElementSibling;
+    btn.onclick = () => {
+      let expanded = btn.getAttribute('aria-expanded') === 'true';
+      btn.setAttribute('aria-expanded', !expanded);
+      target.hidden = expanded;
+    };
+  });
+})();
+
+function scrollToSection(selector, offset = 0) {
+  const element = document.querySelector(selector);
+  const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+  const offsetPosition = elementPosition - offset;
+  window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+  });
+}
