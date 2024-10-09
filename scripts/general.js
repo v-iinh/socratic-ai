b94b4e14d73905c4243e5ea51066845edf3d1f94
v-iinh <officiallyvt@gmail.com>
2024-10-08 20:46:08 -0400

    updated chat log

---
+++ scripts/general.js
@@ -0,0 +1,18 @@
+function scrollToBottom() {
+    const aiContainer = document.querySelector('.chat_history');
+    aiContainer.scrollTop = aiContainer.scrollHeight;
+}
+
+{/* <div class="message ai">
+<div class="pfp">
+    <i class="fas fa-robot"></i>
+</div>
+<div class="messageText"></div>
+</div>
+
+<div class="message user">
+<div class="pfp">
+    <i class="fas fa-user"></i>
+</div>
+<div class="messageText"></div>
+</div> */}
