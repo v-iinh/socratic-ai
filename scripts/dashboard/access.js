3c90319599f8d61ead5977e9e3b223d2a3973f68
Vinh <145184639+v-iinh@users.noreply.github.com>
2024-10-16 17:26:53 -0400

    updated login system

--- scripts/dashboard/access.js
+++ scripts/dashboard/access.js
@@ -1,6 +1,7 @@
 const register = document.getElementById('register')
 const login = document.getElementById('login')
 
+const age = document.getElementById('age')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
@@ -16,33 +17,65 @@ login.addEventListener('click', function(event) {
 });
 
 function registerUser(){
+    const ageInput = age.value
     const usernameInput = username.value.toLowerCase();
     const emailInput = email.value.toLowerCase();
     const passwordInput = password.value.toLowerCase(); 
 
     users.orderByChild('username').equalTo(usernameInput).once('value', snapshot => {
         if(snapshot.exists()){
-            console.log("Username Exists");
+            authText(register, "Invalid", "Register")
         } else {
-            users.push({
-                username: usernameInput,
-                eamil: emailInput,
-                password: passwordInput
-            });
+            if (authRegister(ageInput, usernameInput, emailInput, passwordInput)) {
+                users.push({
+                    username: usernameInput,
+                    eamil: emailInput,
+                    password: passwordInput,
+                    age: ageInput,
+                    admin: false
+                });
+
+                window.location = "../../redirects/dashboard/staff.html"
+            } else {
+                authText(register, "Invalid", "Register")
+            }
         }
     })
 }
 
 function loginUser(){
+    const ageInput = age.value
     const usernameInput = username.value.toLowerCase();
+    const emailInput = email.value.toLowerCase();
     const passwordInput = password.value.toLowerCase(); 
     
     users.orderByChild('username').equalTo(usernameInput).once('value', snapshot => {
-        snapshot.forEach(childSnapshot => {
-            const userData = childSnapshot.val();
-            if(userData.password === passwordInput){
-                console.log("Works")
-            }
-        })
+        if(snapshot.exists() && authRegister(ageInput, usernameInput, emailInput, passwordInput)){
+            snapshot.forEach(childSnapshot => {
+                const userData = childSnapshot.val();
+                if(userData.password === passwordInput){
+                    if(userData.admin == false){
+                        window.location = "../../redirects/dashboard/staff.html"
+                    } else {
+                        window.location = "../../redirects/dashboard/admin.html"
+                    }
+                } else {
+                    authText(login, "Invalid", "Login")
+                }
+            })
+        } else {
+            authText(login, "Invalid", "Login")
+        }
     })
+}
+
+function authText(btn, msg, reset){
+    btn.innerHTML = msg;
+    setTimeout(() => {
+        btn.innerHTML = reset;
+    }, 1000);
+}
+
+function authRegister(age, username, email, password){
+    return ((age !== "" && age <= 17) && username !== "" && email !== "" && password !== "");
 }
