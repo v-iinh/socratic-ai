fe5b6bceb091a1fd54957a409f3673113170b90a
Vinh <officiallyvt@gmail.com>
2024-10-16 12:19:25 -0400

    added input values for login

--- scripts/dashboard/access.js
+++ scripts/dashboard/access.js
@@ -1,9 +1,48 @@
 const register = document.getElementById('register')
+const login = document.getElementById('login')
 
-register.addEventListener('click', function() {
-    users.push({
-        username: 'gracehop',
-        date_of_birth: 'December 9, 1906',
-        full_name: 'Grace Hopper'
-    });
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+register.addEventListener('click', function(event) {
+    event.preventDefault();
+    registerUser()
 });
+
+login.addEventListener('click', function(event) {
+    event.preventDefault();
+    loginUser()
+});
+
+function registerUser(){
+    const usernameInput = username.value.toLowerCase();
+    const emailInput = email.value.toLowerCase();
+    const passwordInput = password.value.toLowerCase(); 
+
+    users.orderByChild('username').equalTo(usernameInput).once('value', snapshot => {
+        if(snapshot.exists()){
+            console.log("Username Exists");
+        } else {
+            users.push({
+                username: usernameInput,
+                eamil: emailInput,
+                password: passwordInput
+            });
+        }
+    })
+}
+
+function loginUser(){
+    const usernameInput = username.value.toLowerCase();
+    const passwordInput = password.value.toLowerCase(); 
+    
+    users.orderByChild('username').equalTo(usernameInput).once('value', snapshot => {
+        snapshot.forEach(childSnapshot => {
+            const userData = childSnapshot.val();
+            if(userData.password === passwordInput){
+                console.log("Works")
+            }
+        })
+    })
+}
