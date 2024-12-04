7928f0452c6cbfce70a5762e2dada04f71fe0470
Vinh <officiallyvt@gmail.com>
2024-12-04 12:45:58 -0500

    removed unload function

--- scripts/dashboard/lobby.js
+++ scripts/dashboard/lobby.js
@@ -2,19 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     addUserWaiting();
 });
 
-window.addEventListener("beforeunload", function(e) {
-    e.returnValue = true;
-});
-
-window.addEventListener("unload", function() {
-    removeUserWaiting();
-});
-
-function performCleanup() {
-    navigator.sendBeacon('/api/cleanup');
-    console.log("Cleanup function executed.");
-}
-
 function addUserWaiting() {
     if (sessionStorage.getItem('waiting') !== 'true') {
         sessions.once('value', (snapshot) => {
@@ -29,23 +16,19 @@ function addUserWaiting() {
 }
 
 function removeUserWaiting() {
-    const userPosition = sessionStorage.getItem('count'); 
-
-    if (!userPosition) {
-        console.error('No position found in sessionStorage.');
-        return;
-    }
+    const userPosition = sessionStorage.getItem('count');
 
     sessions.orderByChild('position').equalTo(parseInt(userPosition)).once('value', (snapshot) => {
-            snapshot.forEach((childSnapshot) => {
-                const sessionKey = childSnapshot.key; 
-                sessions.child(sessionKey).remove().then(() => {
-                    console.log(`Session ${sessionKey} removed successfully.`);
-                }).catch((error) => {
-                    console.error('Error removing session:', error);
-                });
+        snapshot.forEach((childSnapshot) => {
+            const sessionKey = childSnapshot.key; 
+            sessions.child(sessionKey).remove().then(() => {
+                console.log(`Session ${sessionKey} removed successfully.`);
+            }).catch((error) => {
+                console.error('Error removing session:', error);
             });
         });
+        snapshot.onDisconnect().remove()
+    });
         
     sessionStorage.setItem('waiting', 'false');
 }
