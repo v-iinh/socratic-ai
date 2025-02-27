a21380ed85565bbd2ff17b11c3255bd67f74e334
Vinh <145184639+v-iinh@users.noreply.github.com>
2025-02-27 12:33:31 -0500

    todo list + file setup for moderation features

---
+++ scripts/dashboard/moderation.js
@@ -0,0 +1,13 @@
+// TODO // 
+
+// In admin panel, create an Ongoing Sessions tab. 
+// For sessions tabs in admin panel, have the buttons save the ID to session storage and redirect to moderation.html
+// In moderation.html, read the session storage and save the ID
+// Check whether the session is active or inactive
+
+// If active, show the close option
+// If inactive, show the finetuning options
+
+// Finetuning algorithm 
+// Idea: More prompting?
+// Idea: Lower the temperature the more similar a question is to another
