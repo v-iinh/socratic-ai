069b804c12117eea096c4ef86d33135ff79155f5
Vinh <145184639+v-iinh@users.noreply.github.com>
2025-03-12 16:27:48 +0000

    active and inactive session controls

--- scripts/dashboard/moderation.js
+++ scripts/dashboard/moderation.js
@@ -1,8 +1,3 @@
-// Check whether the session is active or inactive
-
-// If active, show the close option
-// If inactive, show the finetuning options
-
 // Finetuning algorithm 
 // Idea: More prompting?
 // Idea: Lower the temperature the more similar a question is to another
@@ -11,14 +6,26 @@ const messages = document.getElementsByClassName('messages')[0];
 const sessionId = sessionStorage.getItem('selected');
 const archiveSession = archive.child(sessionId);
 
+const finetuneControls = document.getElementById('finetune-controls');
+const endControls = document.getElementById('end-controls');
+
 archiveSession.on('value', (snapshot) => {
     Array.from(messages.querySelectorAll('.message')).forEach(child => child.remove());
     const data = snapshot.val();
+
     data.messages.forEach(element => {
         const text = element.text;
         const role = element.role;
         addMessage(text, role);
     });
+
+    if (data.active) {
+        finetuneControls.style.display = 'none';
+        endControls.style.display = 'flex';
+    } else {
+        finetuneControls.style.display = 'flex';
+        endControls.style.display = 'none';
+    }
 });
 
 function addMessage(text, role) {
